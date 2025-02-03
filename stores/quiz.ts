8bf16276a8a07d9e2aaa4784a47c4c6c176ea333
Vladislav-UZH <perebziak.vladislav@gmail.com>
2025-02-03 19:18:12 +0200

    feature(store)

--- stores/quiz.ts
+++ stores/quiz.ts
@@ -32,27 +32,69 @@ export const useQuizStore = defineStore("quiz", () => {
 
   const quizzes = ref<Quiz[]>([]);
 
-  function synchronize(context, value) {
-    context = value;
+  function _synchronize(
+    context: object | object[],
+    value: object,
+    id?: number
+  ) {
+    if (!id) {
+      context = value;
+      return;
+    }
+
+    if (id) {
+      context = (context as object[]).map((el) => {
+        if (el.id === id) {
+          el = value;
+        }
+        return el;
+      });
+    }
   }
 
-  function addQuiz(quiz: Quiz) {}
-  function updateQuiz(id: number, data) {}
-  function getQuiz(id: number) {}
-  function deleteQuiz(id: number) {}
-  function updateCurrentQuiz(data) {}
+  function addQuiz(quiz: Quiz) {
+    quizzes.value.push(quiz);
+  }
 
-  function addQuestion(question: Question) {}
-  function updateQuestion(quizStackId: number, data) {}
-  function updateCurrentQuestion(data) {}
+  function getQuiz(id: number) {
+    return quizzes.value.find((el) => el.id === id);
+  }
+  function deleteQuiz(id: number) {
+    quizzes.value = quizzes.value.filter((el) => el.id !== id);
+  }
+
+  function updateCurrentQuiz(id: number, data: Partial<Quiz>) {
+    currentQuiz.value = { ...currentQuiz.value, ...data };
+
+    _synchronize(quizzes, data, id);
+  }
+
+  function addQuestion(question: Question) {
+    quizzes.value = quizzes.value.map((el) => {
+      if (el.id === question.quizStackId) {
+        el.questions.push(question);
+
+        if (question.quizStackId === currentQuiz.value.id) {
+          _synchronize(currentQuiz.value.questions, el.questions);
+        }
+      }
+      return el;
+    });
+  }
+
+  function updateCurrentQuestion(quizStackId: number, data: Partial<Question>) {
+    currentQuestion.value = { ...currentQuestion.value, ...data };
+
+    _synchronize(quizzes.value, data, quizStackId);
+  }
 
   return {
     quizzes,
     currentQuiz,
     currentQuestion,
 
-    quiz: {},
-    question: {},
+    quiz: { addQuiz, getQuiz, deleteQuiz, updateCurrentQuiz },
+    question: { addQuestion, updateCurrentQuestion },
     option: {},
   };
 });
