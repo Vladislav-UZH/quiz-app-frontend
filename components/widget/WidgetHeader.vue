74d016d2198387f86f0ef34f0ef51ba98cf3f067
Vladislav-UZH <perebziak.vladislav@gmail.com>
2025-03-25 11:23:06 +0200

    fix

--- components/widget/WidgetHeader.vue
+++ components/widget/WidgetHeader.vue
@@ -3,6 +3,12 @@ import { useAuthFeatures } from "~/features/auth";
 import IconQuiz from "/public/svg/icon_quiz.svg";
 
 const auth = useAuthFeatures();
+
+function handleLogout() {
+  localStorage.clear();
+
+  useRouter().push("/auth");
+}
 </script>
 <template>
   <header class="header">
@@ -11,9 +17,13 @@ const auth = useAuthFeatures();
       <h1 class="app-name">QuizApp</h1>
     </router-link>
 
-    <p>
-      Welcome <span class="text__title user">, {{ auth.user.username }} !</span>
-    </p>
+    <div style="display: flex; gap: 12px; align-items: center">
+      <UiButton @click="handleLogout">Logout</UiButton>
+      <p>
+        Welcome
+        <span class="text__title user">, {{ auth.user.username }} !</span>
+      </p>
+    </div>
   </header>
 </template>
 <style scoped>
